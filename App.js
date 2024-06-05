b9de9f5578f0cc2864b01ee82f5ad90554656460
Albert Claudio <117605854+albert-claudio@users.noreply.github.com>
2024-06-05 01:48:25 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+import 'swiper/css'
+import Header from './Pages/Header';
+import Banner from './Pages/Banner';
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Banner/>
+    </>
+  );
+}
+
+export default App;
